ead220395bf7bb3a7f2694075df48063576ce2e9
mike <mikesil121@gmail.com>
2023-07-09 22:54:57 +0800

    Modified and working ajax auto scroll loading

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,10 +2,6 @@ class PostsController < ApplicationController
   def index
     page = params[:page] || 1
     @pagy, @posts = pagy(Post.all, page: page)
-
-    respond_to do |format|
-      format.html
-      format.js { render layout: false }
-    end
+    render :index
   end
 end
