7d5557b00911db8692f96db9d74135b8252b29d1
mike <mikesil121@gmail.com>
2023-07-10 15:30:10 +0800

    Added skeleton template loading while waiting for data

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
   def index
+    # Add sleep function to delay request
+    sleep 2
     page = params[:page] || 1
     @pagy, @posts = pagy(Post.all, page: page)
     render :index
